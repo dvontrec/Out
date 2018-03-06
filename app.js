3960bd0ace4fda78e3a99bc64fe19fa744c9a981
Dvontre Coleman <dvontrec@gmail.com>
2018-03-06 05:17:10 -0500

    Add ejs

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ var express = require("express"),
 	port 	= 8000,
 	app 	= express();
 
+//sets view enjine to ejs
+app.set("view engine", "ejs");
+
+
 
 //***************************
 //		BASIC ROUTES
 //***************************
+//index route.  redirects to homepage
 app.get("/", function(req, res)
 {
 	res.send("it works");
 });
 
-app.get("/still", function(req, res)
+// Redirects to the home route
+app.get("/home", function(req, res)
 {
 	res.send("the future looks bright");
 });
