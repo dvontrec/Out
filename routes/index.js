58db756f4c3f1f06fbe8f219e47a02210515c3d0
Dvontre Coleman <dvontrec@gmail.com>
2018-03-10 23:50:39 -0500

    Add 404 page

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,12 @@ router.get("/", function(req, res)
 	res.render("landing"); //render landing page for start up
 });
 
+//catch all
+router.get("/*", function(req, res)
+{
+	res.render("404");
+});
+
 
 //exports all routes
 module.exports=router;
